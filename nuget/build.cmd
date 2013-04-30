8cb40af3c5c8efe50c4e417ebeb486f012325c6e
Brian Lachniet <blachniet@gmail.com>
2013-04-30 10:11:41 -0400

    Fixed errors in NuGet package - missing css and unnecessary files

--- nuget/build.cmd
+++ nuget/build.cmd
@@ -11,10 +11,10 @@ copy ..\build\js\bootstrap-datetimepicker.min.js Content\Scripts
 
 :: Copy content into temporary directory
 mkdir Content\Content
-copy ..\build\bootstrap-datetimepicker.min.css Content\Content
+copy ..\build\css\bootstrap-datetimepicker.min.css Content\Content
 
 :: Pack up the NuGet package
-NuGet.exe pack
+NuGet.exe pack -Exclude nuget.exe;build.cmd;README.md
 
 :: Delete the temporary content folder
 rd Content /s /q
